cf92c261b95ecef101bd03c4307d7b4e7de5cf5b
bossoff <rajiatlive@gmail.com>
2022-08-29 00:45:56 +0100

    module1

---
+++ app.js
@@ -0,0 +1,3 @@
+var greetMe = require('./greet');
+
+greetMe();
---
+++ greet.js
@@ -0,0 +1,8 @@
+console.log("Hi Bossoff");
+
+let greetMe = function(){
+    console.log('Hi! Bossoff');
+}
+
+
+module.exports = greetMe
