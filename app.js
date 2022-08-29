bffb8f6d578fa8e5ab06fa01562adf2ab5d6e392
bossoff <rajiatlive@gmail.com>
2022-08-29 18:25:32 +0100

    module2 object2

--- app.js
+++ app.js
@@ -32,8 +32,11 @@ var john = new Person('John', 'Doe');
 john.greet();
 
 var jane = new Person('Jane', 'Doe');
+
 jane.greet();
 
 console.log(john.__proto__);
+
 console.log(jane.__proto__);
+
 console.log(john.__proto__ === jane.__proto__);
