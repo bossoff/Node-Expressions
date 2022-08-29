169ac41ad6b4502c52966ba0202b1e06b3154918
bossoff <rajiatlive@gmail.com>
2022-08-29 16:17:36 +0100

    module2 object

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 var greetMe = require('./greet');
 
 greetMe();
+
+// Building Object 
+var person = {
+    'firstname': "Bossoff",
+    'lastname': "Rajih",
+    'greeting': function(){
+        console.log("Hello "+this.firstname+" "+this.lastname);
+    }
+}
+
+person.greeting();
