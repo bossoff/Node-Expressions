a9eef171f5d7967f9c06d1136823f3062f4203be
bossoff <rajiatlive@gmail.com>
2022-08-29 18:21:48 +0100

    module2 object1

--- app.js
+++ app.js
@@ -12,3 +12,28 @@ var person = {
 }
 
 person.greeting();
+
+
+
+// Object Property and prototype
+
+function Person(firstname, lastname) {
+	
+	this.firstname = firstname;
+	this.lastname = lastname;
+	
+}
+
+Person.prototype.greet = function() {
+	console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+};
+
+var john = new Person('John', 'Doe');
+john.greet();
+
+var jane = new Person('Jane', 'Doe');
+jane.greet();
+
+console.log(john.__proto__);
+console.log(jane.__proto__);
+console.log(john.__proto__ === jane.__proto__);
