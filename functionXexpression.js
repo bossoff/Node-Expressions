0af9c11e7e2b9e84edfca288dbc15335efbd3b7a
bossoff <rajiatlive@gmail.com>
2022-08-30 12:18:42 +0100

    function expression by putting and accessing value.

---
+++ functionXexpression.js
@@ -0,0 +1,27 @@
+
+c
+
+(function () {
+
+    let firstname = "Raji";
+
+    console.log(firstname);
+    
+}());
+
+let firstname = "Bossoff";
+
+console.log(firstname);
+
+
+// function expression by putting and accessing value
+
+(function (lastname) {
+
+    let firstname = "Raji";
+
+    console.log(firstname);
+    console.log(lastname);
+    
+}('Kings'));
+
