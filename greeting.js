8a6e6be7b92de901fd0d01a5ee5e3dfdbe5668f1
bossoff <rajiatlive@gmail.com>
2022-08-30 14:30:18 +0100

    greetings4

--- greeting.js
+++ greeting.js
@@ -13,11 +13,22 @@
 
 // greeting 3
 
+// function Greetr() {
+// 	this.greeting = 'Hello world!!';
+// 	this.greet = function() {
+// 		console.log(this.greeting);
+// 	}
+// }
+
+// module.exports = new Greetr();
+
+// greeting 4
+
 function Greetr() {
-	this.greeting = 'Hello world!!';
+	this.greeting = 'Hello world!!!';
 	this.greet = function() {
 		console.log(this.greeting);
 	}
 }
 
-module.exports = new Greetr();
+module.exports = Greetr;
