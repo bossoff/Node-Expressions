1c1aee5f9415faf32cbea9fedb20d1d80c2d6ad6
bossoff <rajiatlive@gmail.com>
2022-08-30 14:37:16 +0100

    Module Patterns

---
+++ ModulePatterns.js
@@ -0,0 +1,26 @@
+
+// Lets do more reqiure function
+
+// stage1 
+
+// const greet = require('./greeting');
+
+// greet()
+
+// Stage2
+
+// const greet = require('./greeting').greet;
+
+// greet();
+
+
+// Stage3
+
+const greet = require('./greeting');
+
+greet.greet();
+
+
+// var Greet4 = require('./greeting');
+// var grtr = new Greet4();
+// grtr.greet();
